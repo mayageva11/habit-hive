69d7ffce5ae340859917fdd8b4aeba1688dda09f
maya Geva <mayageva11@cs.colman.ac.il>
2024-05-31 04:04:27 +0300

    my posts screen

--- src/firebase.js
+++ src/firebase.js
@@ -23,7 +23,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-
-export { auth, firestore };
